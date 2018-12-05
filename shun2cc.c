39a5c4a01fd2b560727dbfd7cf5e0f352b4ef77d
hondo-s <shunsuke.hondo@gmail.com>
2018-12-05 22:57:45 +0900

    Add +, -

--- shun2cc.c
+++ shun2cc.c
@@ -8,10 +8,32 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global _main\n");
     printf("_main:\n");
-    printf("  MOV rax, %d\n", atoi(argv[1]));
+
+    // strtol moves p by 1 after reading
+    printf("  MOV rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++; // Skip +
+            printf("  ADD rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++; // Skip -
+            printf("  SUB rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Unexpected character: %c\n", *p);
+        return 1;
+    }
+
     printf("  RET\n");
     return 0;
 }
